c54635d31df364b6b5686090a087b181c694c95d
Dorian Buck <dorianbuck@gmail.com>
2021-11-02 21:53:22 +0100

    init

--- App.js
+++ App.js
@@ -1,10 +1,33 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
 
-app.get('/', (req, res) => {
-  console.table(req.query)
-  res.send('<h1>Hello Silverspin!</h1>')
-})
+const cors = require("cors");
 
-app.listen(port, () => console.warn(`The audience is listening on port ${port}!`))
+const pool = require("./config");
+
+const app = express();
+
+const getBooks = (request, response) => {
+  pool.query("SELECT * FROM books;", (error, results) => {
+    response.status(200).json({ books: results.rows });
+  });
+};
+
+const createBook = (request, response) => {
+  const { author, title } = request.body;
+  pool.query(
+    "INSERT INTO books (author, title) VALUES ($1, $2)",
+    [author, title],
+    () => {
+      response.status(201).json({ message: "success" });
+    }
+  );
+};
+
+app
+  .use(express.json())
+  .use(express.urlencoded({ extended: true }))
+  .use(cors({ credentials: true, origin: ["http://localhost:3473"] }));
+
+app.route("/").get(getBooks).post(createBook);
+
+app.listen(3001, () => console.log("Server is listeing on port 3001"));
