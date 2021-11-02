bda12be8fce5c24bef42d9d90db4dbd1662fe17c
Dorian Buck <dorianbuck@gmail.com>
2021-11-02 22:30:30 +0100

    adds config for deployment

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
-require("dotenv").config();
+require('dotenv').config()
 
-const { Pool } = require("pg");
+const { Pool } = require('pg')
 
-const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
+const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
 
 const pool = new Pool({
   connectionString: connectionString,
-  ssl: false,
-});
+  ssl: false
+})
 
-module.exports = pool;
+module.exports = pool
